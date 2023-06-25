9dceed8b0bbb7ba5500a169412a49e6be66df8f8
Jhy-An Chen <jhyan.chen1@ucalgary.ca>
2023-06-25 02:43:22 -0700

    Apply html classes to generated grid for styling

--- sketch.js
+++ sketch.js
@@ -3,8 +3,10 @@ function makeGrid(width, height) {
 
   for (i = 0; i < height; i++) {
     const rowContainer = document.createElement("div");
+    rowContainer.classList.add("sketchbook-row");
     for (j = 0; j < width; j++) {
       const node = document.createElement("div");
+      node.classList.add("sketchbook-node");
       rowContainer.appendChild(node);
     }
     container.appendChild(rowContainer);
